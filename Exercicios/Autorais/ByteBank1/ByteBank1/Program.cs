4758f8bffc8aaf6a0031db42389914677f590d47
Ewerthon-izi <ewer.izi@gmail.com>
2022-12-15 16:06:49 -0300

    insert new user and delete a user

--- Exercicios/Autorais/ByteBank1/ByteBank1/Program.cs
+++ Exercicios/Autorais/ByteBank1/ByteBank1/Program.cs
@@ -1,4 +1,53 @@
-﻿namespace ByteBank1 {
+﻿using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+
+namespace ByteBank1 {
+
+    
+    public class User : userBase
+    {
+        public User()
+        {
+            Console.WriteLine(" Bem vindo\nEntre com seus dados");
+            Console.Write("Nome: ");
+            this.nome = Console.ReadLine();
+            Console.Write("senha: ");
+            this.password = Console.ReadLine();
+            Console.Write("email: ");
+            this.email = Console.ReadLine();
+            Console.Write("telefone: ");
+            this.phone = Console.ReadLine();
+            Console.Write("cpf: ");
+            this.cpf = Console.ReadLine();
+        }
+
+        public User newUser()
+        {
+            Console.WriteLine(" Bem vindo\nEntre com seus dados");
+            Console.Write("Nome: ");
+            this.nome = Console.ReadLine();
+            Console.Write("senha: ");
+            this.password = Console.ReadLine();
+            Console.Write("email: ");
+            this.email = Console.ReadLine();
+            Console.Write("telefone: ");
+            this.phone = Console.ReadLine();
+            Console.Write("cpf: ");
+            this.cpf = Console.ReadLine();
+            /*
+             Verificar se o cpf é valido, usar dps
+            if (!(Regex.IsMatch(this.cpf, "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})")))
+            */
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + this.nome + "\nEmail: " + this.email + "\nTelefone: " + this.phone + "\nCPF: " + this.cpf + "\n-----------------\n";
+        }
+        
+    }
 
     public class Program {
 
@@ -15,9 +64,9 @@
 
             Console.WriteLine("Antes de começar a usar, vamos configurar alguns valores: ");
 
-            Console.Write("Digite a quantidade de Usuários: ");
-            int quantidadeDeUsuarios = int.Parse(Console.ReadLine());
-
+            List<User> usuarios = new List<User>();
+            
+            string cpfAux;
             int option;
 
             do {
@@ -26,20 +75,39 @@
 
                 Console.WriteLine("-----------------");
 
+                foreach (var i in usuarios)
+                {
+                    Console.WriteLine(i);
+                }
+
+                Console.WriteLine("-----------------");
+
                 switch (option) {
                     case 0:
                         Console.WriteLine("Estou encerrando o programa...");
                         break;
                     case 1:
-                        Console.WriteLine("Deveria estar inserindo um novo usario!");
+                        usuarios.Add(new User());                     
+                        break;
+                    case 2:
+                        Console.Write("digite o cpf do usuario a deletar: ");
+                        cpfAux = Console.ReadLine();
+                        if(usuarios.RemoveAll(x => x.cpf == cpfAux) >= 1)
+                            Console.WriteLine("Usuario removido com sucesso");
+                        else
+                            Console.WriteLine("Usuario não encontrado");
                         break;
                 }
 
                 Console.WriteLine("-----------------");
 
             } while (option != 0);
-            
-            
+
+            Console.WriteLine(usuarios.Count());
+            foreach(var i in usuarios)
+            {
+                Console.WriteLine(i);
+            }
 
         }
 
---
+++ Exercicios/Autorais/ByteBank1/ByteBank1/userBase.cs
@@ -0,0 +1,29 @@
+﻿namespace ByteBank1
+{
+    public class userBase
+    {
+        public string email { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public double saldo { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+
+
+    }
+}
