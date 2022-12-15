4c162da24bf626ec1e9f2314ecf46550f233cc39
Ewerthon-izi <ewer.izi@gmail.com>
2022-12-15 17:26:29 -0300

    add tranferation, deposit and withdraw operation

--- Exercicios/Autorais/ByteBank1/ByteBank1/Program.cs
+++ Exercicios/Autorais/ByteBank1/ByteBank1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using System;
 using System.Collections.Generic;
+using System.Transactions;
 
 namespace ByteBank1 {
 
@@ -21,30 +22,32 @@ namespace ByteBank1 {
             Console.Write("cpf: ");
             this.cpf = Console.ReadLine();
         }
-
-        public User newUser()
+        /*
+         Verificar se o cpf é valido, usar dps
+        if (!(Regex.IsMatch(this.cpf, "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})")))
+        */
+        public bool retirarSaldo(double valor)
         {
-            Console.WriteLine(" Bem vindo\nEntre com seus dados");
-            Console.Write("Nome: ");
-            this.nome = Console.ReadLine();
-            Console.Write("senha: ");
-            this.password = Console.ReadLine();
-            Console.Write("email: ");
-            this.email = Console.ReadLine();
-            Console.Write("telefone: ");
-            this.phone = Console.ReadLine();
-            Console.Write("cpf: ");
-            this.cpf = Console.ReadLine();
-            /*
-             Verificar se o cpf é valido, usar dps
-            if (!(Regex.IsMatch(this.cpf, "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})")))
-            */
-            return this;
+            if (this.saldo - valor >= 0)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void AddSaldo(double valor)
+        {
+            this.saldo += valor;
         }
 
         public override string ToString()
         {
-            return "Nome: " + this.nome + "\nEmail: " + this.email + "\nTelefone: " + this.phone + "\nCPF: " + this.cpf + "\n-----------------\n";
+            return "Nome: " + this.nome + "\nEmail: " + this.email + "\nTelefone: " + this.phone + "\nCPF: " + this.cpf + "\nSaldo: " + this.saldo + "\n-----------------\n";
         }
         
     }
@@ -56,8 +59,22 @@ namespace ByteBank1 {
             Console.WriteLine("2 - Deletar um usuário");
             Console.WriteLine("3 - Detalhes de um usuário");
             Console.WriteLine("4 - Total armazenado no banco");
+            Console.WriteLine("5 - transferencia bancaria");
+            Console.WriteLine("6 - Depositar");
+            Console.WriteLine("7 - Sacar");
             Console.WriteLine("0 - Para sair do programa");
             Console.Write("Digite a opção desejada: ");
+        }
+
+        static string Tranferencia(User owner, User destiny, double valor)
+        {
+            if (owner.retirarSaldo(valor))
+            {
+                destiny.AddSaldo(valor);
+                return "Tranferencia realizada com sucesso";
+            }
+            else
+                return "Saldo insuficiente";
         }
 
         public static void Main(string[] args) {
@@ -66,19 +83,14 @@ namespace ByteBank1 {
 
             List<User> usuarios = new List<User>();
             
-            string cpfAux;
+            string cpfAux1, cpfAux2;
+            double valorTAux;
             int option;
+            User objectAux1, objectAux2;
 
             do {
                 ShowMenu();
                 option = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("-----------------");
-
-                foreach (var i in usuarios)
-                {
-                    Console.WriteLine(i);
-                }
 
                 Console.WriteLine("-----------------");
 
@@ -87,27 +99,82 @@ namespace ByteBank1 {
                         Console.WriteLine("Estou encerrando o programa...");
                         break;
                     case 1:
-                        usuarios.Add(new User());                     
+                        usuarios.Add(new User());
                         break;
                     case 2:
                         Console.Write("digite o cpf do usuario a deletar: ");
-                        cpfAux = Console.ReadLine();
-                        if(usuarios.RemoveAll(x => x.cpf == cpfAux) >= 1)
+                        cpfAux1 = Console.ReadLine();
+                        if (usuarios.RemoveAll(x => x.cpf == cpfAux1) >= 1)
                             Console.WriteLine("Usuario removido com sucesso");
                         else
                             Console.WriteLine("Usuario não encontrado");
+                        break;
+                    case 3:
+                        Console.Write("digite o cpf do usuario a consultar: ");
+                        cpfAux1 = Console.ReadLine();
+                        objectAux1 = (User)usuarios.FirstOrDefault(x => x.cpf == cpfAux1);
+                        if (objectAux1 != null)
+                            Console.WriteLine(objectAux1);
+                        else
+                            Console.WriteLine("Usuario não encontrado");
+                        break;
+                    case 4:
+                        Console.WriteLine(usuarios.Count());
+                        break;
+                    case 5:
+                        //Como o sistema não possui um sistema de login, o cpf do usuario deve ser informado, caso possuisse um login o usuario nao precisaria informar seu cpf
+                        Console.Write("Digite o seu cpf: ");
+                        cpfAux1 = Console.ReadLine();
+                        Console.Write("Digite o cpf do usuario a receber a transferencia: ");
+                        cpfAux2 = Console.ReadLine();
+                        Console.Write("Digite o valor da transferencia: ");
+                        valorTAux = double.Parse(Console.ReadLine());
+                        objectAux1 = (User)usuarios.FirstOrDefault(x => x.cpf == cpfAux1);
+                        if (objectAux1 == null) {
+                            Console.WriteLine("Usuario que fara a transferencia não encontrado");
+                            break;
+                        }
+                        objectAux2 = (User)usuarios.FirstOrDefault(x => x.cpf == cpfAux2);
+                        if (objectAux2 == null) {
+                            Console.WriteLine("Destinatario não encontrado");
+                            break;
+                        }
+                        Console.WriteLine(Tranferencia(objectAux1, objectAux2, valorTAux));
+                        break;
+                    case 6:
+                        Console.Write("Digite o cpf da conta a ser depositada: ");
+                        cpfAux1 = Console.ReadLine();
+                        objectAux1 = (User)usuarios.FirstOrDefault(x => x.cpf == cpfAux1);
+                        if (objectAux1 == null)
+                        {
+                            Console.WriteLine("Usuario não encontrado");
+                            break;
+                        }
+                        Console.Write("Valor a ser adicionado: ");
+                        valorTAux = double.Parse(Console.ReadLine());
+                        objectAux1.AddSaldo(valorTAux);
+                        break;
+                    case 7:
+                        Console.Write("Digite o cpf da conta a ser sacada: ");
+                        cpfAux1 = Console.ReadLine();
+                        objectAux1 = (User)usuarios.FirstOrDefault(x => x.cpf == cpfAux1);
+                        if (objectAux1 == null)
+                        {
+                            Console.WriteLine("Usuario não encontrado");
+                            break;
+                        }
+                        Console.Write("Digite o valor do saque: ");
+                        valorTAux = double.Parse(Console.ReadLine());
+                        if (objectAux1.retirarSaldo(valorTAux))
+                            Console.WriteLine("Saque realizado com sucesso");
+                        else
+                            Console.WriteLine("Saldo insuficiente");
                         break;
                 }
 
                 Console.WriteLine("-----------------");
 
             } while (option != 0);
-
-            Console.WriteLine(usuarios.Count());
-            foreach(var i in usuarios)
-            {
-                Console.WriteLine(i);
-            }
 
         }
 
